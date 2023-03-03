e9336113acbcf589bafb290c832c8b44131172bf
andrew <deasonam@gmail.com>
2023-03-03 17:09:33 -0600

    some javascript

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // TODO: add code here
+async function requestHandler() {
+    const response = await fetch( "https://handlers.education.launchcode.org/static/astronauts.json");
+    const json = await response.json();
+    return json;
+}
+
+window.addEventListener( "load", async ( event ) => {
+    let json = await requestHandler();
+    const container = document.getElementById( "container" );
+
+    json.forEach( item => {
+        const astronaut = document.createElement( "div" );
+        astronaut.className = "astronaut";
+
+        const bio = ( document.createElement( "div" ));
+        bio.className = "bio";
+
+        const name = document.createElement( "h3" );
+        name.innerHTML = item.firstName + " " + item.lastName;
+
+        const ul = document.createElement( "ul" );
+
+        container.appendChild( astronaut );
+        astronaut.appendChild( bio );
+        bio.appendChild( name );
+        bio.appendChild();
+
+    })
+})
+
+
