9beea8f176a093047d44a11c6d0dbbf3ce203b2c
andrew <deasonam@gmail.com>
2023-03-03 18:18:55 -0600

    more javascript

--- script.js
+++ script.js
@@ -12,20 +12,37 @@ window.addEventListener( "load", async ( event ) => {
     json.forEach( item => {
         const astronaut = document.createElement( "div" );
         astronaut.className = "astronaut";
+        container.appendChild( astronaut );
 
         const bio = ( document.createElement( "div" ));
         bio.className = "bio";
+        astronaut.appendChild( bio );
 
         const name = document.createElement( "h3" );
-        name.innerHTML = item.firstName + " " + item.lastName;
+        name.innerHTML = `${item.firstName} ${item.lastName}`; 
+        bio.appendChild( name );
 
         const ul = document.createElement( "ul" );
+        bio.appendChild( ul );
 
-        container.appendChild( astronaut );
-        astronaut.appendChild( bio );
-        bio.appendChild( name );
-        bio.appendChild();
+        for (let i = 0; i < 3; i++) {
+            const li = document.createElement( "li" );
+            
+            let bioCategories = {
+                0: `Hours in space: ${item.hoursInSpace}`, 
+                1: `Active: ${item.active}`,
+                2: "Skills: "
+            }
 
+            for ( index in item.skills ) {
+                bioCategories[2] += `${item.skills[index]}, `
+            }
+
+            bioCategories[2] = bioCategories[2].slice( 0, bioCategories[2].length - 2 );
+
+            li.innerHTML = `${bioCategories[i]}`;
+            ul.appendChild( li );
+        }
     })
 })
 
